dca9da087c34a8c97a6bbd779d65a86d2d44a8da
yogapriya28 <95758260+yogapriya28@users.noreply.github.com>
2021-12-21 12:35:49 +0530

    Update day6.rb

--- day6.rb
+++ day6.rb
@@ -167,3 +167,89 @@ bulls_on_parade = Song.new(["They rally around tha family",
 happy_bday.sing_me_a_song()
 
 bulls_on_parade.sing_me_a_song()
+class Animal
+end
+
+## ??
+class Dog < Animal
+
+  def initialize(name)
+    ## ??
+    @name = name
+  end
+end
+
+## ??
+class Cat < Animal
+
+  def initialize(name)
+    ## ??
+    @name = name
+  end
+end
+
+## ??
+class Person
+
+  def initialize(name)
+    ## ??
+    @name = name
+
+    ## Person has-a pet of some kind
+    @pet = nil
+  end
+
+  attr_accessor :pet
+end
+
+## ??
+class Employee < Person
+
+  def initialize(name, salary)
+    ## ?? hmm what is this strange magic?
+    super(name)
+    ## ??
+    @salary = salary
+  end
+
+end
+
+## ??
+class Fish
+end
+
+## ??
+class Salmon < Fish
+end
+
+## ??
+class Halibut < Fish
+end
+
+
+## rover is-a Dog
+rover = Dog.new("Rover")
+
+## ??
+satan = Cat.new("Satan")
+
+## ??
+mary = Person.new("Mary")
+
+## ??
+mary.pet = satan
+
+## ??
+frank = Employee.new("Frank", 120000)
+
+## ??
+frank.pet = rover
+
+## ??
+flipper = Fish.new()
+
+## ??
+crouse = Salmon.new()
+
+## ??
+harry = Halibut.new()
